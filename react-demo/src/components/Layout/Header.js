e1899d8059e76d845908b1d7eefc48596b100768
李中敬 <471883209@qq.com>
2025-11-20 22:18:57 +0800

    优化顶部

--- react-demo/src/components/Layout/Header.js
+++ react-demo/src/components/Layout/Header.js
@@ -3,16 +3,9 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTheme } from '../../context/ThemeContext';
 import { useAuth } from '../../context/AuthContext';
-import { IconButton,Span } from '../UI';
+import { IconButton, Span } from '../UI';
 import Button from '../UI/Button';
 import styles from './Header.module.css';
-
-
-
-
-
-
-
 
 const Header = ({ title = "React-Demo" }) => {
   const { user, logout } = useAuth(); // 直接使用 context 提供的 logout 函数
@@ -30,27 +23,51 @@ const Header = ({ title = "React-Demo" }) => {
 
       <div className={styles.headerActions}>
         <IconButton
-          icon="#icon-home"
+          icon="#icon-shouye3"
           onClick={handleBackToHome}
           title="返回首页"
           variant="ghost"
           size="medium"
         />
-        
-        {user && (
+        <IconButton
+          icon="#icon-user-01"
+          onClick={handleBackToHome}
+          title={user.username || user.email || '用户'}
+          variant="ghost"
+          size="medium"
+
+        />
+
+        {/* {user && (
           <Span>
             {user.username || user.email || '用户'}
           </Span>
-        )}
+        )} */}
 
+        <IconButton
+          icon="#icon-tongzhi7"
+          onClick={handleBackToHome}
+          title="返回首页"
+          variant="ghost"
+          size="medium"
+        />
         {/* 点击时直接调用从 context 来的 logout 函数 */}
-        <Button
+        <IconButton
+          icon="#icon-shezhi2"
           onClick={logout}
-          variant="secondary"
-          size="small"
-        >
-          切换账号
-        </Button>
+          title="返回首页"
+          variant="ghost"
+          size="medium"
+        />
+
+        <IconButton
+          icon="#icon-zhuti1"
+          onClick={logout}
+          title="切换账号"
+          variant="ghost"
+          size="medium"
+        />
+
       </div>
     </header>
   );
