69bba40ef76303771f16b97dbbb964db194fbae8
Sandf123hehe <471883209@qq.com>
2026-03-02 09:06:02 +0800

    修改链接

--- react-demo/src/pages/modules/travel/FirstTime/CarouselTypeEighth.js
+++ react-demo/src/pages/modules/travel/FirstTime/CarouselTypeEighth.js
@@ -8,27 +8,27 @@ const CarouselTypeEighth = () => {
   const items = [
     {
       title: '白帝城',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeEighth/1.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeEighth/1.jpg`
     },
     {
       title: '刘备托孤',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeEighth/2.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeEighth/2.jpg`
     },
     {
       title: '山王坪',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeEighth/3.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeEighth/3.jpg`
     },
     {
       title: '习水',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeEighth/4.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeEighth/4.jpg`
     },
     {
       title: '园博园',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeEighth/5.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeEighth/5.jpg`
     },
     {
       title: '龚滩古镇',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeEighth/6.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeEighth/6.jpg`
     }
   ];
 
--- react-demo/src/pages/modules/travel/FirstTime/CarouselTypeSixth.js
+++ react-demo/src/pages/modules/travel/FirstTime/CarouselTypeSixth.js
@@ -5,27 +5,27 @@ const CarouselTypeSixth = () => {
   const items = [
     {
       title: '园博园',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeSixth/1.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeSixth/1.jpg`
     },
     {
       title: '地心谷',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeSixth/2.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeSixth/2.jpg`
     },
     {
       title: '双凤村',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeSixth/3.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeSixth/3.jpg`
     },
     {
       title: '习水',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeSixth/4.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeSixth/4.jpg`
     },
     {
       title: '女儿城',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeSixth/5.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeSixth/5.jpg`
     },
     {
       title: '丹霞谷',
-      src: `http://121.4.22.55:80/backend/images/OurHomePage/CarouselTypeSixth/6.jpg`
+      src: `http://121.4.22.55:8888/backend/images/OurHomePage/CarouselTypeSixth/6.jpg`
     }
   ];
 
